08d77d0656dd9f9010eee94e8143ffd2ae73b6e8
Roterfluss <roterfluss@gmail.com>
2026-04-10 09:24:39 +0200

    Them Code vao script

---
+++ script.js
@@ -0,0 +1,47 @@
+// === DỮ LIỆU SẢN PHẨM ===
+const produkte = [
+  {
+    name: "Tapioka Perlen",
+    marke: "Boboq",
+    kategorie: "topping",
+    preis: 45.00,
+    einheit: "6 x 3kg"
+  },
+  {
+    name: "Taro Pulver",
+    marke: "Possmei",
+    kategorie: "pulver",
+    preis: 38.50,
+    einheit: "10 x 1kg"
+  },
+  {
+    name: "Fruchtsirup",
+    marke: "Possmei",
+    kategorie: "sirup",
+    preis: 32.00,
+    einheit: "6 x 2.5kg"
+  },
+  {
+    name: "Popping Boba",
+    marke: "Boboq",
+    kategorie: "topping",
+    preis: 52.00,
+    einheit: "4 x 3.2kg"
+  },
+  {
+    name: "Matcha Pulver",
+    marke: "Possmei",
+    kategorie: "pulver",
+    preis: 42.00,
+    einheit: "10 x 1kg"
+  },
+  {
+    name: "Brown Sugar Sirup",
+    marke: "Boboq",
+    kategorie: "sirup",
+    preis: 36.00,
+    einheit: "6 x 2.5kg"
+  }
+];
+
+console.log("Produkte geladen:", produkte.length);
